acedac93dcd80d440f13860ca95b678a667c4b5b
Gustavo Saquetta <gustavosaquetta@gmail.com>
2017-09-13 22:11:03 -0300

    commit
    aplicacao com mais de um estados, simulando os estados de um pc

--- state/multi_state.py
+++ state/multi_state.py
@@ -3,6 +3,46 @@ Created on 13 de set de 2017
 
 @author: gustavosaquetta
 '''
+class ComputerState(object):
+	nome = 'state'
+	allowed = []
+
+	def switch(self, state):
+		if state.name in self.allowed:
+			print('O estado atual: ', self, '. Atualizando estado para: ', state.name)
+			self.__class__ = state
+
+		else:
+			print('Estado atual: ', self, ' tentativa de troca para: ', state.name, 'deu ruim!')
+
+	def __str__(self):
+		return self.name
+
+class Off(ComputerState):
+	name = 'off'
+	allowed = ['on']
+
+class On(ComputerState):
+	name = 'on'
+	allowed = ['off', 'suspend', 'hibernate']
+
+class Suspend(ComputerState):
+	name = 'suspend'
+	allowed = ['on']
+
+class Hibernate(ComputerState):
+	name = 'hibernate'
+	allowed = ['on']
+
+class Computer(object):
+	def __init__(self, model='MAC'):
+		self.model = model
+		self.state = Off()
+
+	def change(self, state):
+		self.state.switch(state)
+
 
 if __name__ == '__main__':
-	pass
+	C = Computer()
+	C.change(On)
